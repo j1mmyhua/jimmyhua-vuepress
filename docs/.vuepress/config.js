cd87742c3dd8cac1d6346e4035c3c17335404f21
jimmyhua <pin.ching.0001@gmail.com>
2023-10-12 20:52:34 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: '網站標題',
+	description: '網頁描述',
+	theme: "reco",        
+	themeConfig: {        
+		type: 'blog'      
+	},
+	head: [                                              
+        ['link', { rel: 'icon', href: '/favicon.ico' }]  
+    ]											
+}
